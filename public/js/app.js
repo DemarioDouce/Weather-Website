0a1207680e5c682f69ed7a4e9cb0dd92ae106b33
DemarioDouce <demario.douce567@gmail.com>
2020-12-23 20:44:54 -0500

    added fetch to client side

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,9 @@
-console.log("loaded");
+fetch("http://localhost:3000/weather?cityName=Toronto").then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data);
+    }
+  });
+});
