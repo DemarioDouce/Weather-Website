89d5b851e066eb07478c5f3b80aa7d5e8a270773
DemarioDouce <demario.douce567@gmail.com>
2020-12-23 22:29:16 -0500

    add UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,25 @@
-fetch("http://localhost:3000/weather?cityName=Toronto").then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data);
-    }
+//document elemet selection
+const weatherForm = document.querySelector("form");
+const input = document.querySelector("input");
+const weatherData = document.querySelector("#weatherData");
+const weatherError = document.querySelector("#weatherError");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let location = input.value;
+  weatherData.textContent = "loading....";
+  fetch(
+    "http://localhost:3000/weather?cityName=" + encodeURIComponent(location)
+  ).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        weatherError.textContent = data.error;
+        weatherData.textContent = "";
+      } else {
+        weatherData.textContent = JSON.stringify(data);
+        weatherError.textContent = "";
+      }
+    });
   });
 });
